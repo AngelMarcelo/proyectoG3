3af4cea49018717bc436765893f1312c01819cda
Fermin Aguilar <ferminab99@gmail.com>
2021-10-29 17:36:45 -0300

    se agregaron las validaciones del formulario de carga de productos

---
+++ js/validaciones.js
@@ -0,0 +1,66 @@
+let marca = document.querySelector('#marca');
+let modelo = document.querySelector('#modelo');
+let codigo = document.querySelector('#codigo');
+let descripcion = document.querySelector('#descripcion');
+let url = document.querySelector('#imagenUrl');
+let alerta = document.querySelector('#alerta');
+let formulario = document.querySelector('#formulario');
+
+marca.addEventListener('blur', () => {
+    validacionTexto(marca);
+})
+modelo.addEventListener('blur', () =>{
+    validacionTexto(modelo);
+})
+codigo.addEventListener('blur', () =>{
+    validacionCodigo(codigo);
+})
+descripcion.addEventListener('blur', () =>{
+    validacionTexto(descripcion);
+})
+url.addEventListener('blur', () =>{
+    validacionUrlImagen(url);
+})
+
+
+
+function validacionTexto(input){
+    if (input.value.trim().length > 0){
+        input.className = 'form-control is-valid';
+        return true;
+    }else{
+        input.className = 'form-control is-invalid';
+        return false;
+    }
+}
+
+function validacionCodigo(input){
+    let codigoUnico = /^[1-9]{3}$/;
+    if (codigoUnico.test(input.value)) {
+        input.className = "form-control is-valid";
+        return true;
+      } else {
+        input.className = "form-control is-invalid";
+        return false;
+      }
+}
+
+function validacionUrlImagen(input) {
+    let urlImagen = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+    if (input.value.trim().length > 0 && urlImagen.test(input.value.trim())) {
+      input.className = "form-control is-valid";
+      return true;
+    } else {
+      input.className = "form-control is-invalid";
+      return false;
+    }
+  }
+
+function validacionFormulario (){
+    if (validacionTexto(marca)&&validacionTexto(modelo)&&validacionCodigo(codigo)&&validacionTexto(descripcion)&&validacionUrlImagen(url)){
+        alerta.className = 'alert alert-danger d-flex align-items-center';
+
+    }else{
+        alerta.className = 'alert alert-danger d-flex align-items-center d-none';
+    }
+}
