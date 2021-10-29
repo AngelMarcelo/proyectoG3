58819acc1dc3eeed2f07553825c902e0f4cf86ed
AngelMarcelo <ammarce@gmail.com>
2021-10-29 01:16:07 -0300

    se agregan ajustes de diseño en admin.html y hoja de estilo, se agrega codigo en pagina dmin.js y se crea pagina autoClass

---
+++ js/autoClass.js
@@ -0,0 +1,10 @@
+/*inicializacion de objeto auto a trave de clases*/
+export class autos {
+    constructor(marca, modelo, codigo, descripcion, imagen){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+       }
+}
