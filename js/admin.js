1c3ec6c6e837166a7772194ddbb5c98965e06803
Gabriel Gomez <gabrielgomez198@gmail.com>
2021-10-31 13:08:13 -0300

    se agrego al index las cards, se creo el boton de eliminar producto de autos, se acomodo los exports e imports en la hoja admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,46 +1,73 @@
 import { autos } from "./autoClass.js";
+import { 
+    validacionTexto,
+    validacionCodigo,
+    validacionFormulario,
+    validacionUrlImagen,
+} from "./validaciones.js";
 
 // se agregan variales
-let  marca = getElementById("#marca") ;
-let modelo = getElementById("#modelo");
-let codigo = getElementById("#codigo");
-let descripcion = getElementById("#descripcion");
-let imagen = getElementById("#imagenUrl");
-let formulario = getElementById("#formularioAutos");
+let marca = document.querySelector('#marca');
+let modelo = document.querySelector('#modelo');
+let codigo = document.querySelector('#codigo');
+let descripcion = document.querySelector('#descripcion');
+let imagenUrl = document.querySelector('#imagenUrl');
+let formulario = document.querySelector('#formulario');
 let arregloAutos= [];
 
+marca.addEventListener('blur', () => {
+    validacionTexto(marca);
+})
+modelo.addEventListener('blur', () =>{
+    validacionTexto(modelo);
+})
+codigo.addEventListener('blur', () =>{
+    validacionCodigo(codigo);
+})
+descripcion.addEventListener('blur', () =>{
+    validacionTexto(descripcion);
+})
+imagenUrl.addEventListener('blur', () =>{
+    validacionUrlImagen(imagenUrl);
+})
 
-function agregarAuto(){
-    // se agrega un nuevo vehiculo
-    let autoNuevo = new autos (
-        marca.value,
-        modelo.value,
-        codigo.value,
-        descripcion.value,
-        imagen.value,
- );
-    console.log(autoNuevo);
-    //agregar datos del auto al arreglo
-    arregloAutos.push(autoNuevo);
-    console.log(arregloAutos);
-    //guardar datos en el localStorage
-    localStorage.setItem("", JSON.stringify(arregloAutos));
-    // se debe limpiar el formulario
-    limpiarFormulario();
-    // se debe cargar los datos en la tabla
-
-    // mostar mensaje al usuario, indicando que se agregó correcctamente
-
+function borrarFilasAutos () {
+    let tabla = document.querySelector("#tablaProducto");
+    tabla.innerHTML = "";
 };
-
-function limpiarFormulario(){
-    formulario.reset();
-    marca.className = "form-control";
-    modelo.className = "form-control";
-    codigo.className ="form-control";
-    descripcion.className = "form-control";
-    imagen.className = "form-control";
-    
-
-}
-
+  
+  window.eliminarAutos = (codigo) => {
+    Swal.fire({
+      title: "¿Esta seguro que desea eliminar este producto?",
+      text: "Una vez eliminado el producto no se puede volver a recuperar",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar producto",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        let filtrarAutos = listaAutos.filter((itemAutos) => {
+          return itemAutos.codigo != codigo;
+        });
+  
+        console.log(filtrarAutos);
+        
+        listaAutos = filtrarAutos;
+        
+        localStorage.setItem("arregloAutos", JSON.stringify(listaAutos));
+        
+        borrarFilas();
+        listaAutos.forEach((itemAutos) => {
+          crearFilas(itemAutos);
+        });
+        Swal.fire(
+          "Producto eliminado",
+          "El producto se elimino correctamente",
+          "success"
+        );
+      }
+    });
+  };
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,32 +1,4 @@
-let marca = document.querySelector('#marca');
-let modelo = document.querySelector('#modelo');
-let codigo = document.querySelector('#codigo');
-let descripcion = document.querySelector('#descripcion');
-let imagenUrl = document.querySelector('#imagenUrl');
-let alerta = document.querySelector('#alerta');
-let formulario = document.querySelector('#formulario');
-
-
-
-marca.addEventListener('blur', () => {
-    validacionTexto(marca);
-})
-modelo.addEventListener('blur', () =>{
-    validacionTexto(modelo);
-})
-codigo.addEventListener('blur', () =>{
-    validacionCodigo(codigo);
-})
-descripcion.addEventListener('blur', () =>{
-    validacionTexto(descripcion);
-})
-imagenUrl.addEventListener('blur', () =>{
-    validacionUrlImagen(imagenUrl);
-})
-
-
-
-function validacionTexto(input){
+export function validacionTexto(input){
     if (input.value.trim().length > 0){
         input.className = 'form-control is-valid';
         return true;
@@ -34,9 +6,9 @@ function validacionTexto(input){
         input.className = 'form-control is-invalid';
         return false;
     }
-}
+};
 
-function validacionCodigo(input){
+export function validacionCodigo(input){
     let codigoUnico = /^[1-9]{3}$/;
     if (codigoUnico.test(input.value)) {
         input.className = "form-control is-valid";
@@ -45,9 +17,9 @@ function validacionCodigo(input){
         input.className = "form-control is-invalid";
         return false;
       }
-}
+};
 
-function validacionUrlImagen(input) {
+export function validacionUrlImagen(input) {
     let urlImagen = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
     if (input.value.trim().length > 0 && urlImagen.test(input.value.trim())) {
       input.className = "form-control is-valid";
@@ -56,13 +28,13 @@ function validacionUrlImagen(input) {
       input.className = "form-control is-invalid";
       return false;
     }
-  }
+  };
 
-function validacionFormulario (){
+export function validacionFormulario (){
     if (validacionTexto(marca)&&validacionTexto(modelo)&&validacionCodigo(codigo)&&validacionTexto(descripcion)&&validacionUrlImagen(imagenUrl)){
         alerta.className = 'alert alert-danger d-flex align-items-center';
 
     }else{
         alerta.className = 'alert alert-danger d-flex align-items-center d-none';
     }
-}
+};
